0d6b41368163dc58840f46260ea9209c63861956
Rodrigo Costa Silva <costarodrigosilva247@gmail.com>
2022-11-08 21:34:41 -0300

    feat: added code config

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const server = express();
 
 server.get('/', (req, res) => {
-  return res.send('Server is running!')
-})
+	return res.send('Server is running!');
+});
 
-export { server }
+export { server };
