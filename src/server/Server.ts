af7b330e6015515b71cb30323c5ab928761e5b5c
Rodrigo Costa Silva <costarodrigosilva247@gmail.com>
2022-11-08 22:38:41 -0300

    added router config

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import { router } from './routes/index';
 
 const server = express();
 
-server.get('/', (req, res) => {
-	return res.send('Server is running!');
-});
+server.use(express.json());
+server.use(router);
 
 export { server };
